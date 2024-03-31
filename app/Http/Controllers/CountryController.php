af03408c3551b3460e4d6d62be6d466e4c9aa973
Bence Farkas <bencefarkas89@gmail.com>
2024-03-31 15:54:46 +0200

    feat: add country controller, route and view

---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class CountryController extends Controller
+{
+    public function index()
+    {        
+        return view('country.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\CountryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [CountryController::class, 'index'])->name('countries');
