bd3811113e7de00d1f32206d79d3bfd57b68c097
Bence Farkas <bencefarkas89@gmail.com>
2024-03-31 13:45:13 +0200

    feat: add js ApiClient

---
+++ resources/js/services/api-client.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:8000/api',
+    timeout: 1000,
+    headers: { "Content-Type": 'application/json' },
+});
+
+class ApiClient
+{
+    get(path, params) {
+        return axiosInstance.get(path, params);
+    }
+}
+
+export default new ApiClient();
