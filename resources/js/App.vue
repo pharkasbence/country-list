89eaf82079422eb6b34279f6298f2690818014a4
Bence Farkas <bencefarkas89@gmail.com>
2024-03-31 22:44:48 +0200

    feat: add login and logout

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,9 +1,34 @@
 <template>
     <div>
-        <country-list-page />
+        <div class="p-4 text-right">
+            <div v-if="isAuthenticated">
+                <span>{{ user.name }}</span>
+                <span class="ml-6"><button @click="logOut">Log out</button>   </span>
+            </div>
+
+            <div v-else>
+                <button @click="login">Log in</button>
+            </div>
+        </div>
+
+        <div v-if="isAuthenticated">
+            <country-list-page />
+        </div>
     </div>
 </template>
 
 <script setup>
+    import { useAuth0 } from '@auth0/auth0-vue';
+
     import CountryListPage from '@pages/CountryListPage.vue';
+
+    const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+
+    function login() {
+        loginWithRedirect();
+    }
+
+    function logOut() {
+        logout({ logoutParams: { returnTo: window.location.origin } });
+    }
 </script>
