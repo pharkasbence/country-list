51d3ba902a4a077685c0e08934b61f34f68892d8
Bence Farkas <bencefarkas89@gmail.com>
2024-03-31 22:44:09 +0200

    feat: install auth0-vue package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,22 @@
 import './bootstrap';
 
 import { createApp } from 'vue';
+import { createAuth0 } from '@auth0/auth0-vue';
 import App from './App.vue';
+
 import '../css/app.css';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.use(
+    createAuth0({
+        domain: import.meta.env.VITE_AUTH0_DOMAIN,
+        clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+        useRefreshTokens: true,
+        cacheLocation: 'localstorage',
+        authorizationParams: {
+            redirect_uri: window.location.origin
+        }
+    })
+)
+.mount('#app');
