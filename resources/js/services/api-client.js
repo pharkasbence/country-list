b465118e59c85bf29b77c264f7b4e906f5be8881
Bence Farkas <bencefarkas89@gmail.com>
2024-04-01 20:14:39 +0200

    fix(api-client): set access token before requests

--- resources/js/services/api-client.js
+++ resources/js/services/api-client.js
@@ -1,16 +1,26 @@
 import axios from 'axios';
 
-const auth0AuthInfoLocalStorageKey = '@@auth0spajs@@::' + import.meta.env.VITE_AUTH0_CLIENT_ID + '::@@user@@';
-const authInfo = JSON.parse(localStorage.getItem(auth0AuthInfoLocalStorageKey));
 const timeout = 3000;
+let accessToken;
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_SERVER_URL + '/api',
     timeout: timeout,
     headers: { 
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + authInfo?.id_token,
     },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+    if (!accessToken) {
+        const auth0AuthInfoLocalStorageKey = '@@auth0spajs@@::' + import.meta.env.VITE_AUTH0_CLIENT_ID + '::@@user@@';
+        const authInfo = JSON.parse(localStorage.getItem(auth0AuthInfoLocalStorageKey));
+        accessToken = authInfo?.id_token;
+    }
+
+    config.headers.Authorization = `Bearer ${accessToken}`;
+
+    return config;
 });
 
 class ApiClient {
