bdfa470ca53f83e62824b0f8e48c226e12f4e07d
Bence Farkas <bencefarkas89@gmail.com>
2024-04-01 19:30:17 +0200

    feat: add api auth0 authentication

--- resources/js/services/api-client.js
+++ resources/js/services/api-client.js
@@ -1,15 +1,19 @@
 import axios from 'axios';
 
+const auth0AuthInfoLocalStorageKey = '@@auth0spajs@@::' + import.meta.env.VITE_AUTH0_CLIENT_ID + '::@@user@@';
+const authInfo = JSON.parse(localStorage.getItem(auth0AuthInfoLocalStorageKey));
+const timeout = 3000;
+
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_SERVER_URL + '/api',
-    timeout: 3000,
+    timeout: timeout,
     headers: { 
         'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + authInfo?.id_token,
     },
 });
 
-class ApiClient
-{
+class ApiClient {
     get(path, params) {
         return axiosInstance.get(path, params);
     }
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Api\CountryController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(CountryController::class)->group(function () {
-    Route::group(['prefix' => 'countries'], function() {
+    Route::group(['prefix' => 'countries', 'middleware' => ['auth']], function() {
         Route::get('/', 'index')->name('api.countries');
     });
 });
